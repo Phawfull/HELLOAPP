31ea968377d6756b01ec06e88bd15d04172e35d1
Phawfull <arush.g.2007@gmail.com>
2026-03-17 14:05:09 +0530

    Implement UC4: Display personalized greetings for multiple command-line arguments

--- HELLOAPP.java
+++ HELLOAPP.java
@@ -2,7 +2,9 @@ public class HELLOAPP {
     
     public static void main(String[] args) {
         if (args.length > 0) {
-            System.out.println("Hello " + args[0]);
+            for (String name : args) {
+                System.out.println("Hello " + name);
+            }
         } else {
             System.out.println("Hello World");
         }
